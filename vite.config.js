f369f303295a4e4afdb0ac07a5e338f32fa8afe7
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-02 22:42:07 +0530

    first commit3

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://api-signup-1.vercel.app/'
-    }
-  }
+  plugins: [react()]
 })
